8d968eda8f6351b5da8707d5f651a4a10bf03f90
Kim, Jeongu <jung2381187@naver.com>
2020-02-06 17:27:05 +0900

    change variable from local to global

--- channel.c
+++ channel.c
@@ -13,15 +13,15 @@ typedef double mat[N1][N2];
 typedef double mat1[N1+1][N2];
 typedef double mat2[N1][N2+1];
 
+mat1 u1, u1_nxt, u1_hat;
+mat2 u2, u2_nxt, u2_hat;
+
+mat1 H1, H1_prv, H1_nxt;
+mat2 H2, H2_prv, H2_nxt;
+
+mat phi, phi_new;
+
 int main(void) {
-    mat1 u1, u1_nxt, u1_hat;
-    mat2 u2, u2_nxt, u2_hat;
-
-    mat1 H1, H1_prv, H1_nxt;
-    mat2 H2, H2_prv, H2_nxt;
-
-    mat phi, phi_new;
-
     // solve for C
 
 }
