1406d96ab2780efe651dccc8124177dfcb83cdcb
Jeongu Kim <jeongukim@kaist.ac.kr>
2021-02-02 16:53:09 +0900

    Every act of creation is first an act of destruction

--- src/ibm3d_fracstep.h
+++
@@ -1,8 +0,0 @@
-#ifndef IBM3D_FRACSTEP_H
-#define IBM3D_FRACSTEP_H
-
-#include "ibm3d.h"
-
-void IBMSolver_iterate(IBMSolver *, int, bool);
-
-#endif
--- src/ibm3d_turb.h
+++
@@ -1,8 +0,0 @@
-#ifndef IBM3D_TURB_H
-#define IBM3D_TURB_H
-
-#include "ibm3d.h"
-
-void IBMSolver_calc_tau_r(IBMSolver *);
-
-#endif
